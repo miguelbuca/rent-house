1ae2431151bf2a00867bb24dc2df322a3153feb3
Miguel Buca <miguelpedrobuca@gmail.com>
2022-09-18 07:34:50 +0100

    chore[app-config]: tailwind theme config

--- App.tsx
+++ App.tsx
@@ -1,10 +1,36 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect } from 'react';
+import { Text, View } from 'react-native';
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    GothamBook: require("./assets/fonts/GothamBook.otf"),
+    GothamBold: require("./assets/fonts/GothamBoldRegular.otf"),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View className='flex justify-center items-center flex-1'>
-      <Text className='text-red-300'>Open up App.tsx to start working on your app!</Text>
+    <View className='flex justify-center items-center flex-1' onLayout={onLayoutRootView}>
+      <Text className='text-primary font-GothamBook'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
